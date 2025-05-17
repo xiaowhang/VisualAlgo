e1678c802d5fe4a734ef1ee036e025f049edd252
xiaowhang <b13528723800@163.com>
2025-05-17 16:21:59 +0800

    🐞 fix(header): 修复无分类时面包屑显示异常
    优化面包屑展示逻辑，仅在存在 category 时显示
    避免无分类页面出现空的面包屑项，提升界面友好性

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -3,8 +3,10 @@
     <el-button link class="flex items-center">
       <el-breadcrumb :separator-icon="ArrowRight" class="!text-xl">
         <el-breadcrumb-item>算法可视化工具</el-breadcrumb-item>
-        <el-breadcrumb-item>{{ $route.meta.category }}</el-breadcrumb-item>
-        <el-breadcrumb-item>{{ $route.meta.title }}</el-breadcrumb-item>
+        <template v-if="$route.meta.category">
+          <el-breadcrumb-item>{{ $route.meta.category }}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ $route.meta.title }}</el-breadcrumb-item>
+        </template>
       </el-breadcrumb>
     </el-button>
   </el-header>
